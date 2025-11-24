fb7438dee490493005785298d15a2272caca6c1d
Kelvin <144894229+KelvinAlviero@users.noreply.github.com>
2025-11-24 21:11:44 +0700

    RadiationDemo MK1
    Added Radiation elements with damage

---
+++ Assets/Characters/Props/Scripts/Radiation.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+
+public class Radiation : MonoBehaviour
+{
+    [Header("Settings")]
+    public float checkInterval = 2.0f; 
+    public float maxDamage = 50f;
+
+    private CircleCollider2D radiationZone;
+    private Transform playerTransform;
+    private float timer = 0f;
+    private bool isPlayerInside = false; 
+
+    // Pulls Circle collider from Unity
+    void Start()
+    {
+        radiationZone = GetComponent<CircleCollider2D>();
+        radiationZone.isTrigger = true; 
+    }
+
+    void Update()
+    {
+        
+        timer += Time.deltaTime;
+
+        // 2. Checker
+        if (timer >= checkInterval)
+        {
+            ThreeSecondCheck();
+            timer = 0f; 
+        }
+    }
+
+    void ThreeSecondCheck()
+    {
+        
+        if (isPlayerInside)
+        {
+            //Does math when inside
+            Debug.Log($"Inside!!");
+            CalculateDamage();
+        }
+        else
+        {
+
+            Debug.Log($"AlphaRads: Not inside");
+        }
+    }
+
+    void CalculateDamage()
+    {
+        float range = radiationZone.radius;
+        
+        // Center of the circle
+        float distance = Vector2.Distance(transform.position, playerTransform.position);
+
+        // Calculator
+        float damagePercent = 1.0f - (distance / range);
+        damagePercent = Mathf.Clamp(damagePercent, 0f, 1.0f);
+
+        float finalDamage = maxDamage * damagePercent;
+
+        Debug.Log($"-- AlphaDamage: {finalDamage:F1} (Dist: {distance:F1}m) --");
+    }
+
+    // Trigger logic
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInside = true;
+            playerTransform = other.transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInside = false;
+            playerTransform = null;
+        }
+    }
+}
